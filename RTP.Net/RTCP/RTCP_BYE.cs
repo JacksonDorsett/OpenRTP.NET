e7605c7ace3f54961a590ba1994ba41d3888357e
jdorsett <jackson.dorsett@wsu.edu>
2021-03-10 22:15:22 -0800

    added BYE and SDES

---
+++ RTP.Net/RTCP/RTCP_BYE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTP.Net.RTCP
+{
+    class RTCP_BYE : RTCP_Body
+    {
+        public RTCP_BYE(uint[] sRC)
+        {
+            SRC = sRC;
+        }
+
+        /// <summary>
+        /// List of sources.
+        /// </summary>
+        public uint[] SRC { get; private set; }
+    }
+}
---
+++ RTP.Net/RTCP/RTCP_SDES.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTP.Net.RTCP
+{
+    /// <summary>
+    /// source description (SDES)
+    /// </summary>
+    class RTCP_SDES : RTCP_Body
+    {
+        public RTCP_SDES(uint sRC, SDESItem[] items)
+        {
+            SRC = sRC;
+            this.items = items;
+        }
+
+        /// <summary>
+        /// first SSRC/CSRC
+        /// </summary>
+        public uint SRC { get; set; }
+
+        /// <summary>
+        /// list of SDES items
+        /// </summary>
+        public SDESItem[] items { get; private set; }
+    }
+}
