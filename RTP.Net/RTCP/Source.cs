5f5321e1465d0b04ecbb6444c71711de5e1a9698
Jackson <jdorsett@eecs.wsu.edu>
2021-03-11 22:35:09 -0800

    Added Update method the source class

--- RTP.Net/RTCP/Source.cs
+++ RTP.Net/RTCP/Source.cs
@@ -8,13 +8,7 @@ namespace RTP.Net.RTCP
     {
         public Source(ushort seq)
         {
-            this.BaseSequence = seq;
-            this.MaxSequence = seq;
-            this.BadSequence = RTPConstants.RTP_SEQ_MOD + 1;
-            this.Cycles = 0;
-            this.Recieved = 0;
-            this.RecievedPrior = 0;
-            this.ExpectedPrior = 0;
+            InitSequence(seq);
         }
         public Source(ushort maxSequence, uint cycles, uint baseSequence, uint badSequence, uint probation, uint recieved, uint expectedPrior, uint recievedPrior, uint transmit, uint jitter)
         {
@@ -79,5 +73,87 @@ namespace RTP.Net.RTCP
         /// estimated jitter
         /// </summary>
         public uint Jitter { get; private set; }
+    
+        public int UpdateSequence(ushort seq)
+        {
+            /// Replace return code with exception??
+            ushort udelta = (ushort)(seq - MaxSequence);
+            const int MAX_DROPOUT = 3000;
+            const int MAX_DISORDER = 100;
+            const int MIN_Sequential = 2;
+
+            /*
+             * Source is not valid until MIN_SEQUENTIAL packets with
+             * sequential sequence numbers have been received.
+             */
+            if (this.Probation != 0)
+            {
+                // Packet is in sequence
+                if (seq == (MaxSequence + 1))
+                {
+                    Probation--;
+                    MaxSequence = seq;
+                    if(Probation == 0)
+                    {
+                        InitSequence(seq);
+                    }
+                    Recieved++;
+                    return 1;
+                }
+                else
+                {
+                    Probation = MIN_Sequential - 1;
+                    MaxSequence = seq;
+                }
+                return 0;
+            }
+            else if (udelta < MAX_DROPOUT)
+            {
+                //in order with permissible gap.
+                if (seq < MaxSequence)
+                {
+                    //Sequence Number wrapped - count another 64k cycle.
+                    Cycles += RTPConstants.RTP_SEQ_MOD;
+                }
+                MaxSequence = seq;
+            }
+            else if (udelta <= RTPConstants.RTP_SEQ_MOD - MAX_DISORDER)
+            {
+                //the sequence made a very large jump
+                if (seq == BadSequence)
+                {
+                    /*
+                    * Two sequential packets -- assume that the other side
+                    * restarted without telling us so just re-sync
+                    * (i.e., pretend this was the first packet).
+                    */
+                    InitSequence(seq);
+                }
+                else
+                {
+                    BadSequence = (uint)((seq + 1) & (RTPConstants.RTP_SEQ_MOD - 1));
+                    return 0;
+                }
+            }
+            else
+            {
+                // duplicate or reorder packet
+                // Implement later
+            }
+            Recieved++;
+            return 1;
+
+        }
+
+        private void InitSequence(ushort seq)
+        {
+            this.BaseSequence = seq;
+            this.MaxSequence = seq;
+            this.BadSequence = RTPConstants.RTP_SEQ_MOD + 1;
+            this.Cycles = 0;
+            this.Recieved = 0;
+            this.RecievedPrior = 0;
+            this.ExpectedPrior = 0;
+        }
     }
 }
