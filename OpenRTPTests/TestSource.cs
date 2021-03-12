5ebaf696da37384538d377d30c09d359c902a9ed
Jackson <jdorsett@eecs.wsu.edu>
2021-03-11 23:10:22 -0800

    Added TestSuite

---
+++ OpenRTPTests/TestSource.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using RTP.Net.RTCP;
+namespace OpenRTPTests
+{
+    public class TestSource
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestInitialization()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- RTP.Net/RTCP/Source.cs
+++ RTP.Net/RTCP/Source.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace RTP.Net.RTCP
 {
-    class Source
+    public class Source
     {
         public Source(ushort seq)
         {
