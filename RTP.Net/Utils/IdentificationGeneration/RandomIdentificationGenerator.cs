f93035e741498fe8ec5fe94689352919771466e5
TorNATO-TAO <nathan.waltz@wsu.edu>
2021-03-16 00:30:00 -0700

    Added serialization support for generating a random 32-bit identifier, basically I was looking into actually making the MD5 because I thought there was a specific way that RTP wanted us to create the hash, but instead they were just providing suggestions on how to get random values with inferior languages such as C that don't have cool libraries. So, I implemented our hashing thing so now we have support for unique 32-bit identifiers for our SDES objects (and maybe others, code reuse and all that!

---
+++ RTP.Net/Utils/IdentificationGeneration/RandomIdentificationGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RTP.Net.Utils.IdentificationGeneration
+{
+    /// <summary>
+    ///     Generates a random 32 bit identifier using
+    ///     MD5 routines.
+    /// </summary>
+    public static class RandomIdentificationGenerator
+    {
+        /// <summary>
+        ///     A pseudorandom number generator.
+        /// </summary>
+        private static readonly Random Random = new Random();
+
+        /// <summary>
+        ///     Our method gets a random 32-bit identifier using
+        ///     MD5.
+        ///     (https://tools.ietf.org/html/rfc1321)
+        ///     https://msdn.microsoft.com/en-us/library/system.security.cryptography.md5%28v=vs.110%29.aspx
+        /// </summary>
+        public static byte[] GetRandomIdentification(SDESType type)
+        {
+            // Creates a new instance of this class, gets random hashcodes
+            var typeCode = type.GetHashCode();
+            var dateTime = DateTime.Now.Ticks;
+
+            // make it more random
+            var inputString = (typeCode * dateTime + Random.Next(dateTime.GetHashCode()));
+
+            using var md5 = MD5.Create();
+            var input = Encoding.ASCII.GetBytes(inputString.ToString());
+            var hash = md5.ComputeHash(input);
+
+            return hash;
+        }
+    }
+}
