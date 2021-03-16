bc1013c085ee42029fbc8b7f2650f766b2840937
TorNATO-TAO <nathan.waltz@wsu.edu>
2021-03-15 18:12:04 -0700

    Added a bunch of documentation, added some constructor fields to more closely follow with required stuff for headers. There is still stuff missing in the RTCPHeader but I will work on implementing that later.

--- RTP.Net/RTCP/RTCPHeader.cs
+++ RTP.Net/RTCP/RTCPHeader.cs
@@ -1,41 +1,154 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RTP.Net.RTCP
 {
     /// <summary>
-    /// RTCP common header word.
+    ///     RTCP common header word.
     /// </summary>
     public class RTCPHeader
     {
-        public RTCPHeader(bool padding, byte count, RTCPType type, uint length)
+        /// <summary>
+        ///     A field that varies by packet type.
+        /// </summary>
+        private int _count;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RTCPHeader" /> class.
+        /// </summary>
+        /// <param name="padding">The padding bit.</param>
+        /// <param name="count">The byte representing the count.</param>
+        /// <param name="type">The type enum representing the type of the RTCPType.</param>
+        /// <param name="length">The length of the RTCP packet.</param>
+        public RTCPHeader(bool padding, byte count, RTCPType type, uint length, uint ssrc, uint timeStamp,
+            uint packetCount, uint octetCount)
         {
-            Padding = padding;
-            Count = count;
-            Type = type;
-            Length = length;
+            // Identifies the version of RTP, which is the same in RTCP packets
+            // as in RTP data packets.  The version defined by this specification
+            // is two (2). (https://tools.ietf.org/html/rfc3550)
+            this.Version = RTPVersion.TWO;
+
+            // If the padding bit is set, this individual RTCP packet contains
+            // some additional padding octets at the end which are not part of
+            // the control information but are included in the length field.  The
+            // last octet of the padding is a count of how many padding octets
+            // should be ignored, including itself (it will be a multiple of
+            // four).  Padding may be needed by some encryption algorithms with
+            // fixed block sizes.  In a compound RTCP packet, padding is only
+            // required on one individual packet because the compound packet is
+            // encrypted as a whole for the method in Section 9.1.  Thus, padding
+            // MUST only be added to the last individual packet, and if padding
+            // is added to that packet, the padding bit MUST be set only on that
+            // packet.  This convention aids the header validity checks described
+            // in Appendix A.2 and allows detection of packets from some early
+            // implementations that incorrectly set the padding bit on the first
+            // individual packet and add padding to the last individual packet.
+            // (https://tools.ietf.org/html/rfc3550)
+            this.Padding = padding;
+
+            // A field that varies by packet type
+            this.ReceptionReportCount = count;
+            this.Type = type;
+            this.Length = length;
+            this.SSRC = ssrc;
+            this.TimeStamp = timeStamp;
+            this.PacketCount = packetCount;
+            this.OctetCount = octetCount;
         }
 
-        RTPVersion Version { get => RTPVersion.Two; } //protocol version
+        /// <summary>
+        ///     The RTP version that we are currently using.
+        /// </summary>
+        public RTPVersion Version { get; }
 
-        public bool Padding { get; private set; } //padding flag
+        /// <summary>
+        ///     If the padding bit is set, this individual RTCP packet contains
+        ///     some additional padding octets at the end which are not part of
+        ///     the control information but are included in the length field.  The
+        ///     last octet of the padding is a count of how many padding octets
+        ///     should be ignored, including itself (it will be a multiple of
+        ///     four).  Padding may be needed by some encryption algorithms with
+        ///     fixed block sizes.  In a compound RTCP packet, padding is only
+        ///     required on one individual packet because the compound packet is
+        ///     encrypted as a whole for the method in Section 9.1.  Thus, padding
+        ///     MUST only be added to the last individual packet, and if padding
+        ///     is added to that packet, the padding bit MUST be set only on that
+        ///     packet.  This convention aids the header validity checks described
+        ///     in Appendix A.2 and allows detection of packets from some early
+        ///     implementations that incorrectly set the padding bit on the first
+        ///     individual packet and add padding to the last individual packet.
+        ///     (https://tools.ietf.org/html/rfc3550#section-6.1)
+        /// </summary>
+        protected bool Padding { get; }
 
-        public byte Count //varies by packet type
+        /// <summary>
+        ///     The number of reception report blocks contained in this packet.  A
+        ///     value of zero is valid.
+        /// </summary>
+        protected int ReceptionReportCount
         {
-            get
-            {
-                return Count;
-            }
+            get => this._count;
+
             private set
             {
-                if (value >= 1 << 5) throw new ArgumentOutOfRangeException("input data must be 5 bytes");
-                Count = value;
+                if (value >= 1 << 5)
+                    throw new ArgumentOutOfRangeException("Count", value, "input data must be 5 bytes");
+                this._count = value;
             }
         }
 
-        public RTCPType Type { get; private set; } //RTCP packet type
+        /// <summary>
+        ///     The SSRC field identifies the synchronization source.  This
+        ///     identifier SHOULD be chosen randomly, with the intent that no two
+        ///     synchronization sources within the same RTP session will have the
+        ///     same SSRC identifier.
+        ///     Although the probability of multiple sources choosing the same identifier is
+        ///     low, all RTP implementations must be prepared to detect and
+        ///     resolve collisions.
+        /// </summary>
+        public uint SSRC { get; }
 
-        public uint Length { get; private set; } //pkt len in words, w/o this word
+        /// <summary>
+        ///     Gets the type of the RTCP packet.
+        /// </summary>
+        public RTCPType Type { get; }
+
+        /// <summary>
+        ///     Gets the length of the RTCP packet.
+        /// </summary>
+        public uint Length { get; }
+
+        /// <summary>
+        ///     Corresponds to the same time as the NTP timestamp (above), but in
+        ///     the same units and with the same random offset as the RTP
+        ///     timestamps in data packets.  This correspondence may be used for
+        ///     intra- and inter-media synchronization for sources whose NTP
+        ///     timestamps are synchronized, and may be used by media-independent
+        ///     receivers to estimate the nominal RTP clock frequency.  Note that
+        ///     in most cases this timestamp will not be equal to the RTP
+        ///     timestamp in any adjacent data packet.  Rather, it MUST be
+        ///     calculated from the corresponding NTP timestamp using the
+        ///     relationship between the RTP timestamp counter and real time as
+        ///     maintained by periodically checking the wallclock time at a
+        ///     sampling instant.
+        /// </summary>
+        protected uint TimeStamp { get; }
+
+        /// <summary>
+        ///     The total number of RTP data packets transmitted by the sender
+        ///     since starting transmission up until the time this SR packet was
+        ///     generated.  The count SHOULD be reset if the sender changes its
+        ///     SSRC identifier
+        /// </summary>
+        protected uint PacketCount { get; }
+
+        /// <summary>
+        ///     The total number of payload octets (i.e., not including header or
+        /// padding) transmitted in RTP data packets by the sender since
+        /// starting transmission up until the time this SR packet was
+        /// generated.  The count SHOULD be reset if the sender changes its
+        /// SSRC identifier.  This field can be used to estimate the average
+        /// payload data rate.
+        /// </summary>
+        protected uint OctetCount { get; }
     }
 }
--- RTP.Net/RTPConstants.cs
+++ RTP.Net/RTPConstants.cs
@@ -1,20 +1,41 @@
 ﻿using RTP.Net.RTCP;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RTP.Net
 {
-    class RTPConstants
+    /// <summary>
+    ///     This class contains constants that will be used throughout
+    ///     the project.
+    /// </summary>
+    public class RTPConstants
     {
-        public static readonly RTPVersion VERSION = RTPVersion.Two;
+        /// <summary>
+        ///     An RTPVersion enumerator representing the versions.
+        /// </summary>
+        public static readonly RTPVersion VERSION = RTPVersion.TWO;
 
+        /// <summary>
+        ///     A readonly unsigned integer representing the
+        ///     RTP_SEQ_MOD. A modulus function for the RTP sequence
+        ///     to check it wraps around the max value to 0
+        /// </summary>
         public static readonly uint RTP_SEQ_MOD = (1 << 16);
 
+        /// <summary>
+        ///     A readonly unsigned integer representing
+        ///     the RTP_MAX_SDS.
+        /// </summary>
         public static readonly uint RTP_MAX_SDS = 255;
 
+        /// <summary>
+        ///     A readonly unsigned short representing the
+        ///     RTCP_VALID_MASK.
+        /// </summary>
         public static readonly ushort RTCP_VALID_MASK = (0xc000 | 0x2000 | 0xfe);
 
+        /// <summary>
+        ///     A readonly unsigned integer representing the
+        ///     RTCP_VALID_VALUE.
+        /// </summary>
         public static readonly uint RTCP_VALID_VALUE = (((uint)VERSION << 14) | (uint)RTCPType.SR);
     }
 }
