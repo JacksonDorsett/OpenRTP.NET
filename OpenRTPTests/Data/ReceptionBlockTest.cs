703ac305cc02fa8f717f1be5f6b5178edb48da74
Jackson <jdorsett@eecs.wsu.edu>
2021-03-16 00:15:47 -0700

    created test for RR block Serialize

--- OpenRTPTests/Data/ReceptionBlockTest.cs
+++ OpenRTPTests/Data/ReceptionBlockTest.cs
@@ -14,6 +14,20 @@ namespace OpenRTPTests.Data
             var b = rr.Serialize();
             //SSRC test
             Assert.AreEqual(100, b[3]);
+            //test fraction.
+            Assert.AreEqual(42, b[4]);
+            //loss test
+            Assert.AreEqual(24, b[6]);
+            Assert.AreEqual(91, b[7]);
+            //last seq test
+            Assert.AreEqual(3, b[10]);
+            Assert.AreEqual(232, b[11]);
+            //jitter test
+            Assert.AreEqual(192, b[15]);
+            // lastsrc test
+            Assert.AreEqual(4, b[19]);
+            // last delay test
+            Assert.AreEqual(7, b[23]);
         }
     }
 }
