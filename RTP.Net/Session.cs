12f540d112f75a0d2a585df976f7460b32d31183
TorNATO-TAO <nathan.waltz@wsu.edu>
2021-03-17 14:48:56 -0700

    Implemented an interval calculating algorithm based on OpenRTP specifications

--- RTP.Net/Session.cs
+++ RTP.Net/Session.cs
@@ -1,47 +1,146 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RTP.Net
 {
-    class Session
+    internal class Session
     {
-        int _currentTime; //the current time;
-        int _tp; //the last time an RTCP packet was transmitted;
-        int _tn; //the next scheduled transmission time of an RTCP packet;
-        uint _prevMembers; //the estimated number of session members at the time tn was last recomputed;
-        uint _members; //the most current estimate for the number of session members;
-        uint _senders; // the most current estimate for the number of senders in the session;
         /// <summary>
-        /// rtcp_bw: The target RTCP bandwidth, i.e., the total bandwidth
-        ///that will be used for RTCP packets by all members of this session,
-        ///in octets per second.This will be a specified fraction of the
-        ///"session bandwidth" parameter supplied to the application at
-        ///startup.
+        ///     The average compound RTCP packet size, in octets,
+        ///     over all RTCP packets sent and received by this participant.The
+        ///     size includes lower-layer transport and network protocol headers
+        ///     (e.g., UDP and IP) as explained in Section 6.2.
         /// </summary>
-        uint _rtcp_bw;
-        bool _we_sent; //Flag that is true if the application has sent data since the 2nd previous RTCP report was transmitted.
+        private uint _avg_rtcp_size;
+
         /// <summary>
-        /// avg_rtcp_size: The average compound RTCP packet size, in octets,
-        ///over all RTCP packets sent and received by this participant.The
-        ///size includes lower-layer transport and network protocol headers
-        ///(e.g., UDP and IP) as explained in Section 6.2.
+        ///     The current time.
         /// </summary>
-        uint _avg_rtcp_size;
+        private DateTime _currentTime;
+
         /// <summary>
-        /// initial: Flag that is true if the application has not yet sent
-        ///an RTCP packet.
+        ///     initial: Flag that is true if the application has not yet sent
+        ///     an RTCP packet.
         /// </summary>
-        bool _initial;
-        public Session()
+        private bool _initial;
+
+        /// <summary>
+        ///     The most current estimate for the number of session members.
+        /// </summary>
+        private uint _members;
+
+        /// <summary>
+        ///     The estimated number of session members at the time the
+        ///     next scheduled transmission time was last recomputed.
+        /// </summary>
+        private uint _prevMembers;
+
+        /// <summary>
+        ///     The target RTCP bandwidth, i.e., the total bandwidth
+        ///     that will be used for RTCP packets by all members of this session,
+        ///     in octets per second.This will be a specified fraction of the
+        ///     "session bandwidth" parameter supplied to the application at
+        ///     startup.
+        /// </summary>
+        private uint _rtcp_bw;
+
+        /// <summary>
+        ///     The most current estimate for the number of senders in the session.
+        /// </summary>
+        private uint _senders;
+
+        /// <summary>
+        ///     The next scheduled transmission time of an RTCP packet.
+        /// </summary>
+        private int _tn;
+
+        /// <summary>
+        ///     The last time an RTCP packet was transmitted.
+        /// </summary>
+        private int _tp;
+
+        /// <summary>
+        ///     Our calculated interval.
+        /// </summary>
+        private double _calculatedInterval;
+
+        /// <summary>
+        ///     Flag that is true if and only if the application has sent data since
+        ///     the second previous RTCP report was transmitted.
+        /// </summary>
+        private bool
+            _we_sent;
+
+        public Session(uint avgRtcpSize)
         {
+            _avg_rtcp_size = avgRtcpSize;
             this._tp = 0;
+            this._currentTime = DateTime.Now;
             this._senders = 0;
             this._prevMembers = 1;
             this._members = 1;
             this._we_sent = false;
             this._initial = true;
+            this.CalculateTransmissionInterval();
+        }
 
+        /// <summary>
+        ///     Calculates the transmission interval of our packets.
+        ///     Source: (https://tools.ietf.org/html/rfc3550#section-6.3)
+        /// </summary>
+        private void CalculateTransmissionInterval()
+        {
+            // our mutable "constant" C
+            double c;
+
+            // our mutable "constant" n
+            double n;
+
+            // our mutable "constant" tMin
+
+            // check if the number of senders is less than or
+            // equal to 25% of the number of members
+            if (this._senders <= 0.25 * this._members && this._rtcp_bw != 0)
+            {
+                // Checks whether or not we sent the packet
+                if (this._we_sent)
+                {
+                    c = this._avg_rtcp_size / (0.25 * this._rtcp_bw);
+                    n = this._senders;
+                } 
+                else
+                {
+                    c = _avg_rtcp_size / (0.75 * this._rtcp_bw);
+                    n = this._members - this._senders;
+                }
+            } 
+            else if (this._rtcp_bw != 0)
+            {
+                // cast to double to perform floating point division
+                c = (double)this._avg_rtcp_size / this._rtcp_bw;
+                n = this._members;
+            }
+            else
+            {
+                throw new ArithmeticException("Cannot divide by zero!");
+            }
+
+            // sets the constant Tmin
+            var minimumTime = (this._initial) ? 2.5 : 5;
+
+            // calculates our interval Td
+            var deterministicCalculatedInterval = Math.Max(minimumTime, n*c);
+
+            // initializes a new random
+            var random = new Random();
+
+            // sets the calculated interval to a number uniformly distributed between 0.5 and 1.5 times
+            // the deterministic calculated interval
+            this._calculatedInterval = random.NextDouble() * deterministicCalculatedInterval * 1.5 + 0.5 * deterministicCalculatedInterval;
+
+            // The resulting value is divided by a constant to compensate for the fact the timer
+            // reconsideration algorithm converges to a value of the RTCP bandwidth below the
+            // intended average.
+            this._calculatedInterval /= Math.E - ((double) 3 / 2);
         }
     }
 }
